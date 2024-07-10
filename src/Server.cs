c0a60783ff8596eb98374da87ec1e69326b306b9
Andy Sabee <asabee@rwbaird.com>
2024-07-10 09:23:48 -0500

    pass stage

--- src/Server.cs
+++ src/Server.cs
@@ -3,10 +3,6 @@ using System.Net.Mail;
 using System.Net.Sockets;
 using System.Text;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
-
-// Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 var socket = server.AcceptSocket(); // wait for client
@@ -15,12 +11,21 @@ byte[] buffer = new byte[1024];
 int received = socket.Receive(buffer); //receive the request text
 string[] portions = ASCIIEncoding.UTF8.GetString(buffer).Split("\r\n"); //split it into its portions (Request line, headers, body)
 
-var reqParts = portions[0].Split(" ");
+var reqParts = portions[0].Split(" "); //split request into its portions(method, uri, httptype)
 var (method, path, httpVer) = (reqParts[0], reqParts[1], reqParts[2]);
-Console.WriteLine(path);
-var response = (path != "/") ? 
-    "HTTP/1.1 404 Not Found\r\n\r\n" : 
+
+var response = "";
+if (path.Contains("echo")){
+    string content = path.Substring(path.IndexOf('/', path.IndexOf('/') + 1));
+    response = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content.Length}\r\n\r\n" + content;
+}
+else
+{
+    response = (path != "/") ?
+    "HTTP/1.1 404 Not Found\r\n\r\n" :
     "HTTP/1.1 200 OK\r\n\r\n";
+}
+
 socket.Send(Encoding.UTF8.GetBytes(response));
 
 socket.Close();
