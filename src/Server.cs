6487d301306176eec157503e018ddae737f72d50
Andy Sabee <asabee@rwbaird.com>
2024-07-16 08:57:06 -0500

    progress on reading header

--- src/Server.cs
+++ src/Server.cs
@@ -3,38 +3,68 @@ using System.Net.Mail;
 using System.Net.Sockets;
 using System.Text;
 
-TcpListener server = new TcpListener(IPAddress.Any, 4221);
-server.Start();
-var socket = server.AcceptSocket(); // wait for client
-
-byte[] buffer = new byte[1024];
-int received = socket.Receive(buffer); //receive the request text
-string[] portions = ASCIIEncoding.UTF8.GetString(buffer).Split("\r\n"); //split it into its portions (Request line, headers, body)
-Console.WriteLine(portions);
-var reqParts = portions[0].Split(" "); //split request into its portions(method, uri, httptype)
-var (method, path, httpVer) = (reqParts[0], reqParts[1], reqParts[2]);
-
-var response = "";
-var content = "";
-if(path == "/")
+internal class Program
 {
-    response = "HTTP/1.1 200 OK\r\n\r\n";
-}
-else if (path.StartsWith("/echo/")){
-    content = path.Substring(6);
-    response = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content.Length}\r\n\r\n{content}";
-}
-else if (path.StartsWith("/user-agent"))
-{
-    content = portions[2].Split(" ")[1];
-    response = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content.Length}\r\n\r\n{content}";
-    Console.WriteLine(response);
-}
-else
-{
-    response = "HTTP/1.1 404 Not Found\r\n\r\n";
+    private static async void Main(string[] args)
+    {
+        TcpListener server = new TcpListener(IPAddress.Any, 4221);
+        server.Start();
+        
+        while (true)
+        {
+            var socket = server.AcceptSocket(); // wait for client
+            _ = Task.Run(() => HandleConnection(socket)); //Handle an individual connection(can run multiple times)
+        }
+
+        void HandleConnection(Socket socket)
+        {
+            try
+            {
+                byte[] buffer = new byte[1024];
+                int received = socket.Receive(buffer); //receive the request text
+
+                string[] portions = ASCIIEncoding.UTF8.GetString(buffer).Split("\r\n"); //split it into its portions (Request line, headers, body)
+                var reqParts = portions[0].Split(" "); //split request into its portions(method, uri, httptype)
+                var (method, path, httpVer) = (reqParts[0], reqParts[1], reqParts[2]);
+
+                var response = "";
+                var content = "";
+                if (path == "/")
+                {
+                    Console.WriteLine("Empty path return");
+                    response = "HTTP/1.1 200 OK\r\n\r\n";
+                }
+                else if (path.StartsWith("/echo/"))
+                {
+                    Console.WriteLine("Echo path return");
+                    content = path.Substring(6);
+                    response = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content.Length}\r\n\r\n{content}";
+                }
+                else if (path.StartsWith("/user-agent"))
+                {
+                    Console.WriteLine("User-agent path return");
+                    content = portions[2].Split(" ")[1];
+                    response = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content.Length}\r\n\r\n{content}";
+                    Console.WriteLine(response);
+                }
+                else
+                {
+                    Console.WriteLine("404 path return");
+                    response = "HTTP/1.1 404 Not Found\r\n\r\n";
+                }
+                Console.WriteLine("Response: " + response);
+                socket.Send(Encoding.UTF8.GetBytes(response));
+            }
+            catch
+            {
+                Console.WriteLine("In catch statement");
+                socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 500 Internal Server Error\r\n\r\n"));
+            }
+            finally
+            {
+                socket.Close();
+            }
+        }
+    }
 }
 
-socket.Send(Encoding.UTF8.GetBytes(response));
-
-socket.Close();
